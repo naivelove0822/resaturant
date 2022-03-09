46411f87f1e4c770ff7f40671c14503f82129000
naivelove0822 <naivelove0822@gmail.com>
2022-03-09 21:11:50 +0800

    refactor: modify express routes and add method-override

---
+++ routes/modules/home.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const router = express.Router()
+
+const Restaurant = require('../../models/Restaurant')
+
+// 首頁瀏覽全部餐廳
+router.get("/", (req, res) => {
+  Restaurant.find()
+    .lean()
+    .then(restaurantsData => res.render("index", { restaurantsData }))
+    .catch(err => console.log(err))
+})
+// 搜尋功能
+router.get('/search', (req, res) => {
+  // if (!req.query.keywords) {
+  //   res.redirect("/")
+  // }
+
+  // const keywords = req.query.keywords 搜尋功能會失效
+
+
+  const keyword = req.query.keyword.trim().toLowerCase()
+
+  Restaurant.find()
+    .lean()
+    .then(restaurantsData => {
+      const filterRestaurantsData = restaurantsData.filter(
+        data =>
+          data.name.toLowerCase().includes(keyword) ||
+          data.category.includes(keyword)
+      )
+      if (filterRestaurantsData.length === 0) {
+        res.redirect('/')
+      } else {
+        res.render('index', { restaurantsData: filterRestaurantsData, keyword: keyword })
+      }
+    })
+    .catch(err => console.log(err))
+})
+
+module.exports = router 
